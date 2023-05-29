e5f83421956bab970a29325ea9edde17c4f5470f
Mailoon <marlongomezm@ielacandelariamedellin.edu.co>
2023-05-29 09:56:20 -0500

    Frontend mentor

---
+++ interactive-rating-component-main/calification.js
@@ -0,0 +1,13 @@
+const element1 = document.getElementById('rating1');
+const element2 = document.getElementById('rating2');
+const element3 = document.getElementById('rating3');
+const element4 = document.getElementById('rating4');
+const element5 = document.getElementById('rating5');
+
+element1.addEventlistener('click',function(){
+element1.classList.add('rating-back')
+element2.classList.add('rating-back')
+element3.classList.add('rating-back')
+element4.classList.add('rating-back')
+element5.classList.add('rating-back')
+})
